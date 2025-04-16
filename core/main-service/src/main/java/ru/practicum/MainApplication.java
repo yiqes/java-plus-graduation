15447f212596e520659025422c34b6fadb89b08e
yiqes <155768368+yiqes@users.noreply.github.com>
2025-04-16 19:35:30 +0300

    fix: fixed checkstyle violation

--- core/main-service/src/main/java/ru/practicum/MainApplication.java
+++ core/main-service/src/main/java/ru/practicum/MainApplication.java
@@ -2,7 +2,6 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import ru.practicum.config.ErrorDecoderConfig;
