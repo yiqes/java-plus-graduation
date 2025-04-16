efad63f01b8f71990f6b2ea6e99609fc859d9a53
yiqes <155768368+yiqes@users.noreply.github.com>
2025-04-16 19:33:33 +0300

    feat: added discovery, config and gateway services

--- core/main-service/src/main/java/ru/practicum/MainApplication.java
+++ core/main-service/src/main/java/ru/practicum/MainApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import ru.practicum.config.ErrorDecoderConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ConfigurationPropertiesScan
+@EnableFeignClients(defaultConfiguration = ErrorDecoderConfig.class, basePackages = "ru.practicum")
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
---
+++ core/main-service/src/main/java/ru/practicum/config/ErrorDecoderConfig.java
@@ -0,0 +1,14 @@
+package ru.practicum.config;
+
+import feign.Feign;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ErrorDecoderConfig {
+    @Bean
+    public Feign.Builder feignBuilder() {
+        return Feign.builder()
+                .errorDecoder(new CustomErrorDecoder());
+    }
+}
