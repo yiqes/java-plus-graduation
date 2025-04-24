ba81633e043df71c67dbfaa12e03021224750368
yiqes <155768368+yiqes@users.noreply.github.com>
2025-04-24 15:01:32 +0300

    gateway fixes

--- core/event-service/src/main/java/ru/practicum/EventApplication.java
+++ core/event-service/src/main/java/ru/practicum/EventApplication.java
@@ -2,13 +2,17 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import ru.practicum.handler.ErrorDecoderConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(defaultConfiguration = ErrorDecoderConfig.class, basePackages = "ru.practicum") 
+@ConfigurationPropertiesScan
+@EnableFeignClients(basePackages = {"ru.practicum.client", "ru.practicum"})
+@ComponentScan(basePackages = {"ru.practicum", "ru.practicum"})
 public class EventApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventApplication.class, args);
--- core/user-service/src/main/java/ru/practicum/UserApplication.java
+++ core/user-service/src/main/java/ru/practicum/UserApplication.java
@@ -2,13 +2,15 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import ru.practicum.handler.ErrorDecoderConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(defaultConfiguration = ErrorDecoderConfig.class, basePackages = "ru.practicum")
+@EnableFeignClients(basePackages = {"ru.practicum.client", "ru.practicum"})
+@ConfigurationPropertiesScan
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
