0b728bdb251a41ba09253accb524402c75f6a973
yiqes <155768368+yiqes@users.noreply.github.com>
2025-05-11 14:52:08 +0300

    fix: resolved reviewer comments

--- core/interaction-api/src/main/java/ru/practicum/dto/event/RecommendedEventDto.java
+++ core/interaction-api/src/main/java/ru/practicum/dto/event/RecommendedEventDto.java
@@ -1,15 +1,18 @@
 package ru.practicum.dto.event;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class RecommendedEventDto {
-    private Long eventId;
-    private double score;
+    Long eventId;
+    double score;
 }
